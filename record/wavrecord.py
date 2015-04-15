32990049542e4246cc5e844f83a75373d93a6147
JKillian <jasonkillian35@gmail.com>
2015-04-15 13:46:09 -0400

    xrange? Really?!?

--- record/wavrecord.py
+++ record/wavrecord.py
@@ -11,54 +11,19 @@ FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
 
-def record_word():
-    """
-    Record a word or words from the microphone and 
-    return the data as an array of signed shorts.
+def record_word_to_file(output_file_name):
+    "Records from the microphone and outputs the resulting data to 'path'"
+    sample_width, data = _record_word()
+    data = pack('<' + ('h'*len(data)), *data)
 
-    Normalizes the audio, trims silence from the 
-    start and end, and pads with 0.5 seconds of 
-    blank sound to make sure VLC et al can play 
-    it without getting chopped off.
-    """
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT, channels=1, rate=RATE,
-        input=True, output=True,
-        frames_per_buffer=CHUNK)
+    wf = wave.open(path, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(sample_width)
+    wf.setframerate(RATE)
+    wf.writeframes(data)
+    wf.close()
 
-    num_silent = 0
-    snd_started = False
-
-    r = array('h')
-
-    while 1:
-        # little endian, signed short
-        snd_data = array('h', stream.read(CHUNK))
-        if byteorder == 'big':
-            snd_data.byteswap()
-        r.extend(snd_data)
-
-        silent = is_silent(snd_data)
-
-        if silent and snd_started:
-            num_silent += 1
-        elif not silent and not snd_started:
-            snd_started = True
-
-        if snd_started and num_silent > 30:
-            break
-
-    sample_width = p.get_sample_size(FORMAT)
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    r = normalize(r)
-    r = trim(r)
-    r = add_silence(r, 0.5)
-    return sample_width, r
-
-def record_for(seconds, output_file_name):
+def record_for_seconds_to_file(seconds, output_file_name):
    p = pyaudio.PyAudio()
 
    stream = p.open(format=FORMAT,
@@ -88,11 +53,58 @@ def record_for(seconds, output_file_name):
    wf.writeframes(b''.join(frames))
    wf.close()
 
-def is_silent(snd_data):
+def _record_word():
+    """
+    Record a word or words from the microphone and 
+    return the data as an array of signed shorts.
+
+    Normalizes the audio, trims silence from the 
+    start and end, and pads with 0.5 seconds of 
+    blank sound to make sure VLC et al can play 
+    it without getting chopped off.
+    """
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+        input=True, output=True,
+        frames_per_buffer=CHUNK)
+
+    num_silent = 0
+    snd_started = False
+
+    r = array('h')
+
+    while 1:
+        # little endian, signed short
+        snd_data = array('h', stream.read(CHUNK))
+        if byteorder == 'big':
+            snd_data.byteswap()
+        r.extend(snd_data)
+
+        silent = _is_silent(snd_data)
+
+        if silent and snd_started:
+            num_silent += 1
+        elif not silent and not snd_started:
+            snd_started = True
+
+        if snd_started and num_silent > 30:
+            break
+
+    sample_width = p.get_sample_size(FORMAT)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    r = _normalize(r)
+    r = _trim(r)
+    r = _add_silence(r, 0.5)
+    return sample_width, r
+
+def _is_silent(snd_data):
     "Returns 'True' if below the 'silent' threshold"
     return max(snd_data) < THRESHOLD
 
-def normalize(snd_data):
+def _normalize(snd_data):
     "Average the volume out"
     MAXIMUM = 16384
     times = float(MAXIMUM)/max(abs(i) for i in snd_data)
@@ -102,9 +114,9 @@ def normalize(snd_data):
         r.append(int(i*times))
     return r
 
-def trim(snd_data):
+def _trim(snd_data):
     "Trim the blank spots at the start and end"
-    def _trim(snd_data):
+    def _real_trim(snd_data):
         snd_started = False
         r = array('h')
 
@@ -118,29 +130,18 @@ def trim(snd_data):
         return r
 
     # Trim to the left
-    snd_data = _trim(snd_data)
+    snd_data = _real_trim(snd_data)
 
     # Trim to the right
     snd_data.reverse()
-    snd_data = _trim(snd_data)
+    snd_data = _real_trim(snd_data)
     snd_data.reverse()
     return snd_data
 
-def add_silence(snd_data, seconds):
+def _add_silence(snd_data, seconds):
     "Add silence to the start and end of 'snd_data' of length 'seconds' (float)"
-    r = array('h', [0 for i in xrange(int(seconds*RATE))])
+    r = array('h', [0 for i in range(int(seconds*RATE))])
     r.extend(snd_data)
-    r.extend([0 for i in xrange(int(seconds*RATE))])
+    r.extend([0 for i in range(int(seconds*RATE))])
     return r
 
-def record_to_file(path):
-    "Records from the microphone and outputs the resulting data to 'path'"
-    sample_width, data = record_word()
-    data = pack('<' + ('h'*len(data)), *data)
-
-    wf = wave.open(path, 'wb')
-    wf.setnchannels(1)
-    wf.setsampwidth(sample_width)
-    wf.setframerate(RATE)
-    wf.writeframes(data)
-    wf.close()
