f131f928e56051db5e40f1fbd5953eaf9e2a3c02
Sam Galambos <hamster_pi@msn.com>
2015-04-15 13:41:02 -0400

    add record_word

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from record import wavrecord
 
-wavrecord.record_for(5, 'data/output.wav')
+# wavrecord.record_for(5, 'data/output.wav')
 
+
+
+if __name__ == '__main__':
+    print("please speak a word into the microphone")
+    wavrecord.record_to_file('demo.wav')
+    print("done - result written to demo.wav")
