d37c10f57d179001953a1f6ffc29b0eef9c16d66
Sam Galambos <hamster_pi@msn.com>
2015-04-29 14:00:42 -0400

    changes, but seems to be less good

--- classifier.py
+++ classifier.py
@@ -17,74 +17,98 @@ class Classifier(object):
         pass
 
     def read_train_files(self):
-        self.fpaths = []
+        """Return
+            - a list of filenames from the data directory
+            - a list of labels for those filenames
+            - a list of ints representing those labels
+            in a tuple"""
+
+        fpaths = []
         labels = []
 
         for fname in glob('data/*'):
-            self.fpaths.append(fname)
+            fpaths.append(fname)
             label = fname.split('/')[1][:-6]
             labels.append(label)
 
-        # self.int_labels is an array of numbers with the correct class of each row in feat, as an int
-        self.int_labels = np.zeros(len(labels))
+        # int_labels is an array of numbers with the correct class of each row in feat, as an int
+        int_labels = np.zeros(len(labels))
         for ind, label in enumerate(set(labels)):
-            self.int_labels[np.array([i for i, _ in enumerate(labels) if _ == label])] = ind
+            int_labels[np.array([i for i, _ in enumerate(labels) if _ == label])] = ind
 
-        print "Read {num_files} files".format(num_files=len(self.int_labels))
+        print "Read {num_files} files".format(num_files=len(int_labels))
+        return (fpaths, labels, int_labels)
 
-    def make_features(self):
+    def make_features(self, fpaths):
+        """Given a list of filenames, return an np array of the features for those filenames"""
 
-        self.feat = []
-        for fname in self.fpaths:
+        feats = []
+        for fname in fpaths:
             mfccs = []
             rate, sig = wav.read(fname)
-            sig = np.array(sig)
-            sig = np.pad(sig, (0, 2.1*rate - len(sig)), mode='constant')
+            sig = np.array(sig[::5])
+            sig = np.pad(sig, (0, .2*2.1*rate - len(sig)), mode='constant')
             fb = np.array(logfbank(sig, rate))
-            self.feat.append(np.transpose(fb))
+            feats.append(np.transpose(fb))
 
-        # self.feat[i] = ith audio file
-        # self.feat[i][j] = jth fbank feature of frame
-        # self.feat[i][j][k] = kth frame of audio file
-        self.feat = np.atleast_3d(self.feat)
+        # feats[i] = ith audio file
+        # feats[i][j] = jth fbank feature of frame
+        # feats[i][j][k] = kth frame of audio file
+        feats = np.atleast_3d(feats)
         
         # The HMM expects probabilities, so we normalize.
         # Without it, we get random guessing
-        for index, val in enumerate(self.feat):
-            self.feat[index] /= self.feat[index].sum(axis=0)
+        for index, val in enumerate(feats):
+            feats[index] /= feats[index].sum(axis=0)
 
+        return feats
 
-    def train(self):
+    def make_models(self, X_train, y_train, word_set):
+        """Train self.models on X_train with y_train as the correct labels (ints) for X_train.
+            word_set (ints) is the list of potential words to train on."""
+        num_states = 2
+        self.models = [gmmhmm(num_states) for word in word_set]
+        self.models = [model.fit(X_train[y_train == word, :, :]) for model, word in zip(self.models, word_set)]
+
+    def predict(self, X_test, y_test):
+
+        ps = [model.transform(X_test) for model in self.models]
+        res = np.vstack(ps)
+        predicted_labels = np.argmax(res, axis=0)
+        return predicted_labels
         
-        self.read_train_files()
 
-        self.make_features()        
+    def train(self, plot=False):
+        
+        fnames, labels, int_labels = self.read_train_files()
 
-        sss = StratifiedShuffleSplit(self.int_labels, test_size=0.2, random_state=0)
+        feats = self.make_features(fnames)        
 
-        for num_states in range(2, 10):
-            for train_index, test_index in sss:
-                # train_index: list of indices, len = (1-test_size)*num_audio_files
-                # test_index: inverse of train_index
-                X_train, X_test = self.feat[train_index, ...], self.feat[test_index, ...]
-                y_train, y_test = self.int_labels[train_index], self.int_labels[test_index]
+        sss = StratifiedShuffleSplit(int_labels, test_size=0.3, random_state=0)
 
-            ys = set(self.int_labels)
-            models = [gmmhmm(num_states) for y in ys]
-            _ = [model.fit(X_train[y_train == y, :, :]) for model, y in zip(models, ys)]
-            ps = [model.transform(X_test) for model in models]
-            res = np.vstack(ps)
-            predicted_labels = np.argmax(res, axis=0)
+        for train_index, test_index in sss:
+            # train_index: list of indices, len = (1-test_size)*num_audio_files
+            # test_index: inverse of train_index
+
+            X_train, X_test = feats[train_index, ...], feats[test_index, ...]
+            y_train, y_test = int_labels[train_index], int_labels[test_index]
+
+            self.make_models(X_train, y_train, set(int_labels))
+            print "models trained!"
+            predicted_labels = self.predict(X_test, y_test)
             missed = (predicted_labels != y_test)
-            print '%d Test accuracy:%.2f percent' % (num_states, (100 * (1 - np.mean(missed))))
+            print 'Test accuracy:%.2f percent' % (100 * (1 - np.mean(missed)))
+            if plot:
+                self.plot(y_test, predicted_labels, list(set(labels)))
+            break
 
 
-    def plot(self):
+    def plot(self, y_test, predicted_labels, word_set):
         cm = confusion_matrix(y_test, predicted_labels)
         plt.matshow(cm, cmap='gray')
         ax = plt.gca()
-        _ = ax.set_xticklabels([" "] + [l[:2] for l in spoken])
-        _ = ax.set_yticklabels([" "] + spoken)
+        _ = ax.set_xticklabels([" "] + [l[4:6] for l in word_set])
+        _ = ax.set_yticklabels([" "] + word_set)
         plt.title('Confusion matrix, single speaker')
         plt.ylabel('True label')
         plt.xlabel('Predicted label')
--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from classifier import Classifier
 
 if __name__ == '__main__':
     clf = Classifier()
-    clf.train()
+    clf.train(plot=True)
+    # clf.predict_word('')
     # clf.plot()
 
 
     # print("please speak a word into the microphone")
-    # wavrecord.record_to_file('data/sam_steal15.wav')
+    # wavrecord.record_word_to_file('data/nate_block03.wav')
     # print("done - result written to demo.wav")
 
